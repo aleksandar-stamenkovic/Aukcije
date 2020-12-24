33170f2747dc1a80a034e6feadff005e126ebc41
aleksandar <acaacaas@gmail.com>
2020-12-24 23:33:56 +0100

    BackEnd init

---
+++ BackEnd/AukcijeAPI/AukcijeAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RedisDataLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AukcijeAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        [HttpGet]
+        public void Get()
+        {
+            Aukcija aukcija = new Aukcija();
+            aukcija.Upisi("key123", "TEST");
+        }
+    }
+}
---
+++ BackEnd/AukcijeAPI/RedisDataLayer/Aukcija.cs
@@ -0,0 +1,19 @@
+﻿using ServiceStack.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedisDataLayer
+{
+    public class Aukcija
+    {
+        readonly RedisClient redis = new RedisClient("localhost");
+
+        public void Upisi(string key, string value)
+        {
+            redis.Set(key, value);
+        }
+    }
+}
