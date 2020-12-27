18b8fe5515d26162744233970b3e08f3acd5d77e
mixy <milan.mixy.stankovic@elfak.rs>
2020-12-28 00:13:54 +0100

    Korisnik atribut(informacija) o list njegovih aukcija

--- BackEnd/AukcijeAPI/RedisDataLayer/KorisnikRedis.cs
+++ BackEnd/AukcijeAPI/RedisDataLayer/KorisnikRedis.cs
@@ -11,13 +11,18 @@ namespace RedisDataLayer
     public class KorisnikRedis
     {
         readonly RedisClient redis = new RedisClient("localhost");
+        AukcijaRedis _ar;
+
+        //objekat _ar jer objekat tipa koji se koristi kako bi
+        //korisnik mogao da pristupa funkcijama klase AukcijaRedis
 
         public void DodajNovogKorisnika(Korisnik k)
         {
             redis.SetEntryInHash(k.Email, "ID", k.ID);
             redis.SetEntryInHash(k.Email, "Password", k.Password);
             redis.SetEntryInHash(k.Email, "Ime", k.Ime);
-            redis.SetEntryInHash(k.Email, "Prezime", k.Prezime);
+            redis.SetEntryInHash(k.Email, "Prezime", k.Prezime);            
+            redis.SetEntryInHash(k.Email, "MojeAukcije", k.Email+ ":MOJEAUKCIJE");//email:MOJEAUKCIJE je kljuc za pribavljanje liste aukcija
             //public void SetRangeInHash(string hashId, IEnumerable<KeyValuePair<string, string>> keyValuePairs);
         }
 
@@ -47,6 +52,29 @@ namespace RedisDataLayer
             return false;
         }
 
+        public void DodajAukcijuKorisniku(string id, string email)
+        {
+            redis.PushItemToList(email + ":MOJEAUKCIJE", id);
+        }
+
+        public List<Aukcija> ProcitajSveAukcijeKorisnika(string email)
+        {
+            List<string> list = redis.GetRangeFromList(email + ":MOJEAUKCIJE", 0, -1);
+            List<Aukcija> listaAukcija = new List<Aukcija>();
+            foreach(string el in list)
+            {
+                listaAukcija.Add(_ar.ProcitajAukciju(el));
+            }
+            return listaAukcija;
+        }
+
+        public bool ObrisiAukcijuKorisnika(string id, string email)
+        {
+            byte[] idbytes = Encoding.ASCII.GetBytes(id);
+            if (redis.LRem(email + ":MOJEAUKCIJE", 0, idbytes) != 0)
+                return true;
+            return false;
+        }
 
     }
 }
