9d9383fcddc0470edbd6d16f40bbe84def4bcea1
mixy <milan.mixy.stankovic@elfak.rs>
2020-12-27 20:42:54 +0100

    Dodaj i procitaj korisnika

--- BackEnd/AukcijeAPI/RedisDataLayer/KorisnikRedis.cs
+++ BackEnd/AukcijeAPI/RedisDataLayer/KorisnikRedis.cs
@@ -11,5 +11,33 @@ namespace RedisDataLayer
     class KorisnikRedis
     {
         readonly RedisClient redis = new RedisClient("localhost");
+
+        public void DodajNovogKorisnika(Korisnik k)
+        {
+            redis.SetEntryInHash(k.Email, "ID", k.ID);
+            redis.SetEntryInHash(k.Email, "Password", k.Password);
+            redis.SetEntryInHash(k.Email, "Ime", k.Ime);
+            redis.SetEntryInHash(k.Email, "Prezime", k.Prezime);
+            //public void SetRangeInHash(string hashId, IEnumerable<KeyValuePair<string, string>> keyValuePairs);
+        }
+
+        public Korisnik ProcitajKorisnika(string email)
+        {
+
+            Dictionary<string, string> dc = redis.GetAllEntriesFromHash(email);            
+
+            Korisnik tmp = new Korisnik();
+            string s;
+            dc.TryGetValue("ID", out s);
+            tmp.ID = s;
+            dc.TryGetValue("Password", out s);
+            tmp.Password = s;
+            dc.TryGetValue("Ime", out s);
+            tmp.Ime = s;
+            dc.TryGetValue("Prezime", out s);
+            tmp.Prezime = s;
+
+            return tmp;
+        }
     }
 }
