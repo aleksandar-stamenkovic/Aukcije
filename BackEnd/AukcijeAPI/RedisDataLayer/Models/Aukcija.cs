23a51761b8de65aeb2fa1bcfcbd506c20ff81dc5
mixy <milan.mixy.stankovic@elfak.rs>
2020-12-27 22:20:24 +0100

    AukcijaRedis osnovne funkcije

--- BackEnd/AukcijeAPI/RedisDataLayer/Models/Aukcija.cs
+++ BackEnd/AukcijeAPI/RedisDataLayer/Models/Aukcija.cs
@@ -10,9 +10,11 @@ namespace RedisDataLayer.Models
     public class Aukcija
     {
         public DateTime Vreme { get; set; }
+        public string ID { get; set; }
         public string Naziv { get; set; }
         public string Opis { get; set; }
         public float Cena { get; set; }
+        public int Trajanje { get; set; }
         public List<Korisnik> Bideri { get; set; }
     }
 }
