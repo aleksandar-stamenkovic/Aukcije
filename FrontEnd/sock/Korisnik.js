77266d286f9ad85598d9a9362b072ef4c4dd6ba8
aleksandar <acaacaas@gmail.com>
2021-01-05 21:02:36 +0100

    registrovanje korisnika js

---
+++ FrontEnd/sock/Korisnik.js
@@ -0,0 +1,30 @@
+export class Korisnik {
+
+    constructor(ime,prezime,email,password) {
+        this.ime=ime;
+        this.prezime=prezime;
+        this.email=email;
+        this.password=password;
+    }
+
+    registrujSe() {
+        const korisnik = new Korisnik();
+        korisnik.ime = document.querySelector("#ime");
+        korisnik.prezime = document.querySelector("#prezime");
+        korisnik.email = document.querySelector("#email");
+        korisnik.password = document.querySelector("#password");
+
+        fetch("https://localhost:44371/Korisnik", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ "ime": korisnik.ime, "prezime": korisnik.prezime,
+                    "cena": korisnik.email, "kolicina": korisnik.password, "id": 55 })
+        }).then(p => {
+            if (p.ok) {
+                console.log("Korisnik dodat u bazu");
+            }
+        });
+    }
+}
